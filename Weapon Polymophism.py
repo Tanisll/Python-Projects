1e83277a1003ea9cb10a6fc30dc55beb525306be
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-08-16 02:43:43 -0600

    assignments

---
+++ Weapon Polymophism.py
@@ -0,0 +1,35 @@
+class Weapon:
+    damage = "1d6"
+    reach = '5ft'
+    weight = '2 lb.'
+    cost = 0
+    dmg_type = 'piercing'
+
+    def Attack(self):
+        X = "Swing your weapon at a target within reach and make an attack roll against their AC rating."
+        Y = "Thrust your weapon at a target within reach and make an attack roll against their AC rating."
+        print(X)
+
+class Dagger(Weapon):
+    damage = '1d4'
+    weight = '1 lb.'
+    cost = '2 gp'
+
+    def Attack(self):
+        X = "Swing your weapon at a target within reach and make an attack roll against their AC rating."
+        Y = "Thrust your weapon at a target within reach and make an attack roll against their AC rating."
+        print(Y)
+
+class Warhammer(Weapon):
+    damage = '1d8'
+    weight = '3 lb.'
+    cost = '15 gp'
+    dmg_type = 'bludgeoning'
+
+    def Attack(self):
+        X = "Swing your weapon at a target within reach and make an attack roll against their AC rating."
+        Y = "Thrust your weapon at a target within reach and make an attack roll against their AC rating."
+        print(X)
+
+
+if __name__ == "__main__":
