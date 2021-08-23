ad3b88977eb85869fd73cf3d449991b6ef71e588
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-08-23 15:44:41 -0600

    after help

--- File Transfer/test.py
+++ File Transfer/test.py
@@ -2,19 +2,27 @@ import shutil, os, time, datetime
 
 
 now = time.time()
-oneDay_ago = now - 60*60*24 #Number of seconds in one day
-destination = '../File Transfer/To-Be-Uploaded/'
+day = 60*60*24 #Number of seconds in one day
+destination = 'C:/Users/Jacob/Documents/GitHub/Python-Projects/File Transfer/To-Be-Uploaded/'
+source = 'C:/Users/Jacob/Documents/GitHub/Python-Projects/File Transfer/Old Files/'
+past = now - day
+print(int(past))
 
-for file in os.listdir('../File Transfer/Old Files/'):
-    if file.endswith ('.txt'):
-        mtime = os.path.getmtime('../File Transfer/Old Files/' + file)
-        ctime = os.path.getctime('../File Transfer/Old Files/' + file)
-        print('File Name: {}'.format(file))
-        print('Last Modified Time: {}'.format(mtime))
-        print('Original Created Time: {}'.format(ctime))
-        print('')
-        if mtime > oneDay_ago:
-            shutil.move(file, destination)
+def last_mod_time(file):
+    return os.path.getmtime(file)
+
+
+for file in os.listdir(source):
+    mtime = os.path.join(source, file)
+    #ctime = os.path.getctime(source + file)
+    print('File Name: {}'.format(file))
+    print('Last Modified Time: {}'.format(mtime))
+    #print('Original Created Time: {}'.format(ctime))
+    print('')
+    if last_mod_time(mtime) > past:
+        destination_file = os.path.join(destination, file)
+        shutil.move(file, destination)
+        
 
 
 
