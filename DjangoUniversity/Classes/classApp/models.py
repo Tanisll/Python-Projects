935771e6bebe40d9a42f659994cfec463af56544
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-08-31 05:05:12 -0600

    first edits. steps 1-4

---
+++ DjangoUniversity/Classes/classApp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.db.models import Model
+from django.db.models import Manager
+
+
+def djangoClasses(models.Model):
+    title = models.CharField(max_length=50, default='', blank=False, null=False)
+    courseNumber = models.IntegerField(max_length=10, default='', blank=False, null=False)
+    instructorName = models.CharField(max_length=80, default='', blank=False, null=False)
+    duration = models.FloatField(max_length=30, default='', blank=False, null=False)
+
+    objects = models.Manager()
+
+
+def __str__(self):
+    return self.name
