34f986d5136ec34b58b869c78561cd8520f66542
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-08-31 22:26:23 -0600

    tried changing the indentation of the djangoClasses class and changed the naming convention of it to CammelCasing. also made sure that this name change was applied to the whole project

--- DjangoUniversity/Classes/classApp/admin.py
+++ DjangoUniversity/Classes/classApp/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import djangoClasses
+from .models import DjangoClasses
 
-admin.site.register(djangoClasses)
+admin.site.register(DjangoClasses)
--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -1,22 +1,21 @@
 from django.db import models
-from models import djangoClasses
 from django.db.models import Model
 from django.db.models import Manager
 
 
-class djangoClasses(models.Model):
+class DjangoClasses(models.Model):
     title = models.CharField(max_length=50, default='', blank=False, null=False)
     courseNumber = models.IntegerField(max_length=10, default='', blank=False, null=False)
     instructorName = models.CharField(max_length=80, default='', blank=False, null=False)
     duration = models.FloatField(max_length=30, default='', blank=False, null=False)
 
-    class1 = djangoClasses(title="Math", courseNumber=13, instructorName="Jerry White", duration= 15.25)
-    class2 = djangoClasses(title="Literature", courseNumber=9, instructorName="Scott Housing", duration= 20.5)
-    class3 = djangoClasses(title="Art II", courseNumber=18, instructorName="Phil Murphy", duration= 12.5)
+class1 = DjangoClasses(title="Math", courseNumber=13, instructorName="Jerry White", duration= 15.25)
+class2 = DjangoClasses(title="Literature", courseNumber=9, instructorName="Scott Housing", duration= 20.5)
+class3 = DjangoClasses(title="Art II", courseNumber=18, instructorName="Phil Murphy", duration= 12.5)
 
-    class1.save()
-    class2.save()
-    class3.save()
+class1.save()
+class2.save()
+class3.save()
 
 def __str__(self):
     return self.name
