1204e244da720d23c0cac02b8276867c4a01880c
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-08-31 22:01:45 -0600

    added objects and saved them

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from models import djangoClasses
 from django.db.models import Model
 from django.db.models import Manager
 
@@ -9,8 +10,13 @@ class djangoClasses(models.Model):
     instructorName = models.CharField(max_length=80, default='', blank=False, null=False)
     duration = models.FloatField(max_length=30, default='', blank=False, null=False)
 
-    objects = models.Manager()
+    class1 = djangoClasses(title="Math", courseNumber=13, instructorName="Jerry White", duration= 15.25)
+    class2 = djangoClasses(title="Literature", courseNumber=9, instructorName="Scott Housing", duration= 20.5)
+    class3 = djangoClasses(title="Art II", courseNumber=18, instructorName="Phil Murphy", duration= 12.5)
 
+    class1.save()
+    class2.save()
+    class3.save()
 
 def __str__(self):
     return self.name
