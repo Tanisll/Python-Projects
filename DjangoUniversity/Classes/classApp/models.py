e4e4ccb364fd19bfc2bf0feef9c07ba395d4bafb
Tanisll <Jacob_W_Loveland@yahoo.com>
2021-09-01 12:39:17 -0600

    finally fixed the issue with DjangoClasses table not showing in the admin console.

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -1,21 +1,13 @@
 from django.db import models
-from django.db.models import Model
-from django.db.models import Manager
 
 
 class DjangoClasses(models.Model):
     title = models.CharField(max_length=50, default='', blank=False, null=False)
-    courseNumber = models.IntegerField(max_length=10, default='', blank=False, null=False)
+    courseNumber = models.IntegerField(default='', blank=False, null=False)
     instructorName = models.CharField(max_length=80, default='', blank=False, null=False)
     duration = models.FloatField(max_length=30, default='', blank=False, null=False)
 
-class1 = DjangoClasses(title="Math", courseNumber=13, instructorName="Jerry White", duration= 15.25)
-class2 = DjangoClasses(title="Literature", courseNumber=9, instructorName="Scott Housing", duration= 20.5)
-class3 = DjangoClasses(title="Art II", courseNumber=18, instructorName="Phil Murphy", duration= 12.5)
-
-class1.save()
-class2.save()
-class3.save()
-
-def __str__(self):
-    return self.name
+    # This will display the 'title' field for each row in the database as opposed to
+    # 'DjangoClasses object (1)'. It will now display Math or Literature
+    def __str__(self):
+        return self.title
